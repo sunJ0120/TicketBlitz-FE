f760e41d36a73818fa9cb6ab91a15f7dad10b60a
sunJ0120 <sspure1214@gmail.com>
2025-12-13 23:41:27 +0900

    chore: 메인 설정들 구성 완료

---
+++ fe/src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+function HomePage() {
+  return (
+      <div className="min-h-screen flex items-center justify-center pt-16">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold text-white mb-4">TICKETBLITZ</h1>
+          <p className="text-zinc-400 text-xl">콘서트 티켓팅 서비스</p>
+        </div>
+      </div>
+  );
+}
+
+export default function App() {
+  return (
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
