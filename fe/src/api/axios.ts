3cd4c4618d1d4d5ab5a57f958d74c5276b8be882
sunJ0120 <sspure1214@gmail.com>
2025-12-13 23:39:49 +0900

    feat: axios 설정 완료

---
+++ fe/src/api/axios.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:8080',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 요청 시 토큰 자동 첨부
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
