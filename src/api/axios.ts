97c961824b183e03d5ed09e22751d97af1f0971d
sunJ0120 <sspure1214@gmail.com>
2025-12-25 23:58:47 +0900

    fix: axios 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://localhost:8080/api/v1',
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
